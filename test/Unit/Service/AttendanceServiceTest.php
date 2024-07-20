12f1bcef6d6b7758690d92561a2c605c9d398d2e
lauro <lauro.appelt@universo.univates.br>
2024-07-20 19:21:34 -0300

    phpunit setup

---
+++ test/Unit/Service/AttendanceServiceTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HyperfTest\Unit\Service;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class AttendanceServiceTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testBatata(): void
+    {
+        self::assertTrue(true);
+    }
+}
